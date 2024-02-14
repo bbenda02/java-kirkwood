0d743ec31cc0981ac5c0b715f9cd4bdf15d583c3
Brian Benda <brianbenda56@gmail.com>
2024-02-14 11:00:55 -0600

    Add Javadoc for Helpers round method

--- CodeDemos/src/main/java/edu/kirkwood/shared/Helpers.java
+++ CodeDemos/src/main/java/edu/kirkwood/shared/Helpers.java
@@ -2,7 +2,16 @@ package edu.kirkwood.shared;
 
 import java.text.DecimalFormat;
 
+
+
 public class Helpers {
+    /**
+     * Formats a double value to a string with the specified number of decimal places.
+     * @param number The double value to be formatted.
+     * @param decimalPlaces The desired number of decimal places.
+     * @return A string representation of the formatted number.
+     * @throws IllegalArgumentException If the number of decimal places is negative.
+     */
     public static String round(double number, int decimalPlaces) {
         if(decimalPlaces < 0) {
             throw new IllegalArgumentException("decimal places must be greater than 0");
